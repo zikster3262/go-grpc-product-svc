052e7c2401a6bc3826ce034f143cc1e092664b5e
zikster <zikster3262@gmail.com>
2022-05-28 15:33:52 +0200

    first commits

--- cmd/main.go
+++ cmd/main.go
@@ -27,7 +27,7 @@ func main() {
 		log.Fatalln("Failed to listing:", err)
 	}
 
-	fmt.Println("Product Svc on", c.Port)
+	fmt.Printf("Server Order service is running on port%v.", c.Port)
 
 	s := services.Server{
 		H: h,
