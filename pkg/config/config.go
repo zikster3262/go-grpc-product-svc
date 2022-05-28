f4054ddf86fcd1d57070900221ed735c1695bc98
zikster <zikster3262@gmail.com>
2022-05-28 16:47:46 +0200

    first commits

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,9 +3,8 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	Port         string `mapstructure:"PORT"`
-	DBUrl        string `mapstructure:"DB_URL"`
-	JWTSecretKey string `mapstructure:"JWT_SECRET_KEY"`
+	Port  string `mapstructure:"PORT"`
+	DBUrl string `mapstructure:"DB_URL"`
 }
 
 func LoadConfig() (config Config, err error) {
